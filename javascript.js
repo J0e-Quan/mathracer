689eb8a5e1664ebd0fceb531af1dd3bdcb9ae6b6
Lim Joe Quan <joequanlim@gmail.com>
2026-01-13 18:36:34 +0800

    Begin planning JS psuedocode

---
+++ javascript.js
@@ -0,0 +1,9 @@
+/*
+GAME MANAGER (IIFE, assign to const during testing)
+*/
+
+/*
+PLAYER MANAGER (IIFE, assign to const during testing)
+Factory Function for player, make 2 players with name const, score vars, roundScore vars, roundTime vars
+
+*/
