fb6840ab20c0e08230421f40dd603922686fce2d
Lim Joe Quan <joequanlim@gmail.com>
2026-03-22 14:37:30 +1300

    Prepare localStorage functions

--- src/storage.js
+++ src/storage.js
@@ -1,7 +1,10 @@
 const settings = {totalQuestions: 10}
 
-export function updateStorage() {
+export function updateStorage(category, value) {
+  settings[category] = value
+  localStorage.setItem("settings", settings)
 }
 
 export function retrieveStorage() {
+  return localStorage.getItem("settings")
 }
