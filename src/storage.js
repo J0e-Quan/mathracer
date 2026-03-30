a016ae4f2895db7f46c117403e386c20d54132e5
Lim Joe Quan <joequanlim@gmail.com>
2026-03-30 18:17:17 +0800

    feat(settings): implement localStorage
    stringify settings object before storing and parse settings object
    before retrieving

--- src/storage.js
+++ src/storage.js
@@ -1,14 +1,16 @@
-let settings = {totalQuestions: 10}
+let settings = { totalQuestions: 10 }
 
 export function updateStorage(category, value) {
   settings[category] = value
-  localStorage.setItem("settings", settings)
+  const settingsString = JSON.stringify(settings)
+  localStorage.setItem("settings", settingsString)
 }
 
 export function retrieveStorage() {
   if (localStorage.getItem("settings") !== null) {
-    settings = localStorage.getItem("settings")
+    settings = JSON.parse(localStorage.getItem("settings"))
+    return settings
   } else {
     settings = { totalQuestions: 10 }
   return settings
-}
+}}
