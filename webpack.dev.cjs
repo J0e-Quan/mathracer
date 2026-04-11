e7ec6061a9332f7bb676ad0a605740cb7d1dac1e
Lim Joe Quan <joequanlim@gmail.com>
2026-04-11 22:27:28 +0800

    refactor(wds): change default wds port to 3000
    8080 kept opening as https, resulting in insecure connection error

--- webpack.dev.cjs
+++ webpack.dev.cjs
@@ -5,7 +5,8 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map',
   devServer: {
-    watchFiles: ['./src/index.html', './src/assets/**/*']
+    watchFiles: ['./src/index.html', './src/assets/**/*'],
+    port: 3000
   },
   module: {
     rules: [
